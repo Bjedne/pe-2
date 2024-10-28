b6b62b34c90e34eb1b799547d03628a6f3ba58cf
Bjedne <126247097+Bjedne@users.noreply.github.com>
2024-10-28 15:13:25 +0100

    Added dev branch and prepare the project for coding

--- project-exam-2/src/App.js
+++ project-exam-2/src/App.js
@@ -1,20 +1,21 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import { Button } from '@mui/material';
 
 function Home() {
   return (
     <div>
-      <Button variant="text">Text</Button>
-<Button variant="contained">Contained</Button>
-<Button variant="outlined">Outlined</Button>
+      <h1>Home</h1>
     </div>
   );
 }
 
+
 function App() {
   return (
     <div id="root">
+      <main>
+      </main>
+      
       <Routes>
         <Route index element={<Home />} />
       </Routes>
