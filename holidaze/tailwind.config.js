6f44964819446ad9309fa9587028885f7b7b3545
Bjedne <126247097+Bjedne@users.noreply.github.com>
2024-11-01 11:39:03 +0100

    Added logo, created icon components and started working to the layout of the header

--- holidaze/tailwind.config.js
+++ holidaze/tailwind.config.js
@@ -2,6 +2,18 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
+    colors: {
+      wood: "#716947",
+      pearl: "#F1F2EE",
+      leaf: "#4B6858",
+      white: "#ffffff",
+      danger: "#B01212 "
+    },
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"],
+      serif: ["Cormorant Infant", "serif"],
+      body: ["Assistant" , "Roboto", "sans-serif"]
+    },
     extend: {},
   },
   plugins: [],
