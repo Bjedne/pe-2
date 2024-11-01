4e4f800d537f689e358686c086c431a45e53c2e5
Bjedne <126247097+Bjedne@users.noreply.github.com>
2024-11-01 11:51:58 +0100

    Created the layout for the pages and made the footer sticky

--- holidaze/src/App.js
+++ holidaze/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import { HamburgerIcon, ProfileIcon } from './components/icons.jsx';
 
 function Header() {
   return (
@@ -15,29 +16,23 @@ function Header() {
   );
 }
 
-
-function HamburgerIcon() {
+function Footer() {
   return (
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="size-8">
-    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-  </svg>
-  )
+    <footer className="bg-wood p-4 text-white text-center">
+      <p>&copy; 2024 Holidaze</p>
+    </footer>
+  );
 }
-
-function ProfileIcon () {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="white" className="size-8">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
-</svg>
-  )
-}
-
 
 function App() {
   return (
-    <main>
+    <div className="flex flex-col min-h-screen">
       <Header />
-    </main>
+      <main className="flex-1 bg-pearl">
+
+      </main>
+      <Footer />
+    </div>
    
     
   );
